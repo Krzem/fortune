a0875c6bd4681dc7588acd21f59b2ff074129206
Krzem <starart@wp.pl>
2010-01-11 02:11:59 +0100

    zmiany

--- app/controllers/education_controller.rb
+++ app/controllers/education_controller.rb
@@ -2,8 +2,7 @@ class EducationController < ApplicationController
   # GET /education
   # GET /education.xml
   def index
-    @education = Education.paginate :page => params[:page], :per_page => 5,
-    :order => 'updated_at DESC'
+    @education = Education.all
 
     respond_to do |format|
       format.html # index.html.erb
